84695d5000dd4dca302f23112c70451091b4e340
Arqwels <danil.kuzmin.1994@gmail.com>
2023-12-17 20:45:17 +0400

    Перешёл на Postgres

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,10 @@
 const { Sequelize } = require('sequelize')
 
 module.exports = new Sequelize(
-  process.env.DB_NAME, // Название БД
-  process.env.DB_USER, // Пользователь
-  process.env.DB_PASSWORD, // Пароль БД
+  process.env.DB_NAME, // database = Название БД
+  process.env.DB_USER, // username = Пользователь
+  process.env.DB_PASSWORD, // password = Пароль БД
   {
-    dialect: 'mysql',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
-  }
-)
+    dialect: 'postgres', 
+});
