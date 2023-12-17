ac503105c0c9361076350005bed9dbcfb2fad0a2
Arqwels <danil.kuzmin.1994@gmail.com>
2023-12-17 22:48:47 +0400

    Заливаю на Vercel

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,14 @@
 const { Sequelize } = require('sequelize')
 
+// module.exports = new Sequelize(
+//   process.env.DB_NAME, // database = Название БД
+//   process.env.DB_USER, // username = Пользователь
+//   process.env.DB_PASSWORD, // password = Пароль БД
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres', 
+// });
+
 module.exports = new Sequelize(
   process.env.DB_NAME, // database = Название БД
   process.env.DB_USER, // username = Пользователь
