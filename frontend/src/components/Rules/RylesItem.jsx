8da0a53e696435f12fe08c1f3d7006abee4b6851
Arqwels <danil.kuzmin.1994@gmail.com>
2024-01-18 21:50:53 +0400

    Сделал страничку Ryles

--- frontend/src/components/Rules/RylesItem.jsx
+++ frontend/src/components/Rules/RylesItem.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import iconRule from '../../images/icon-rule.svg';
+import { useState } from "react";
+import IconRule from "../../assets/images/icons/rule-icon.svg";
+import st from "./Rules.module.scss";
 
 const RylesItem = ({ rule }) => {
-
   const [ isOpen, setIsOpen ] = useState(false);
 
   return (
-  <li className={"rule__item" + (isOpen ? " active" : "")}>
-    <div className="rule__item__head" onClick={() => setIsOpen(!isOpen)}>
-      <p>{rule.id}. {rule.name}</p>
-      <img className="rule__head__img" src={iconRule} alt="not-active-rule" width="12px" height="8px" />
-    </div>
-    <ul className="rule__item__body">
-        {rule.items.map((item, idx) => (
-          <li key={`menu item ${idx}`}>{rule.id}.{item.id} {item.name}</li>
-        ))}
-    </ul>
-  </li>
-  )
-}
+    <li className={`${st.ruleItem}${isOpen ? ` ${st.active}` : ""}`}>
+      <div className={st.head} onClick={() => setIsOpen(!isOpen)}>
+        <p>{rule.id}. {rule.name}</p>
+        <img className={st.headImg} src={IconRule} alt="not-active-rule" width="12px" height="8px" />
+      </div>
+      <ul className={st.body}>
+          {rule.items.map((item, idx) => (
+            <li key={`menu item ${idx}`}>{rule.id}.{item.id} {item.name}</li>
+          ))}
+      </ul>
+    </li>
+  );
+};
 
-export default RylesItem
+export default RylesItem;
