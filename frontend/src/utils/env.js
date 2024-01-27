21b1cc549756cf0a6651ebb283202abda1b17a7a
Arqwels <danil.kuzmin.1994@gmail.com>
2024-01-27 20:19:08 +0400

    Поправка

--- frontend/src/utils/env.js
+++ frontend/src/utils/env.js
@@ -1,2 +1,2 @@
 export const SERVER_URL = "http://localhost:5000/";
-export const RPODUCTION_SERVER_URL = "https://api.shutworld.ru/";
+export const PRODUCTION_SERVER_URL = "https://api.shutworld.ru/";
