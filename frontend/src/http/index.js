48a58946de5cb2fdc3c056bdb1384201ce3d1235
Arqwels <danil.kuzmin.1994@gmail.com>
2024-01-27 20:15:12 +0400

    Поправка в baseURL

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,15 +1,18 @@
 import axios from "axios";
-import { SERVER_URL, RPODUCTION_SERVER_URL } from "../utils/env";
+import { SERVER_URL, PRODUCTION_SERVER_URL } from "../utils/env";
+
+// Определяем базовый URL в зависимости от среды выполнения
+const baseURL = process.env.NODE_ENV === "production" ? PRODUCTION_SERVER_URL : SERVER_URL;
 
 const $api = axios.create({
   withCredentials: true, // Использование куки
-  baseURL: SERVER_URL || RPODUCTION_SERVER_URL
-})
-console.log(SERVER_URL);
-// Перехватчик запросов Axios, перед каждый запросом, добавляется заголовок Authorization с токеном, полученный из Локального хранилища
+  baseURL: baseURL
+});
+
+// Перехватчик запросов Axios, перед каждым запросом добавляется заголовок Authorization с токеном, полученным из Локального хранилища
 $api.interceptors.request.use((config) => {
   config.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
-})
+});
 
 export default $api;
